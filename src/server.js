add2463956e17abf2e97223872f7d10973309d6a
OleksandraStepanova <oleksandra.stepanova@gmail.com>
2024-07-16 00:13:26 +0200

    add index.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,32 @@
 import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+
 
 export default function setupServer() {
     const app = express();
+
+    app.use(cors());
+
+    app.use(
+        pino({
+            transport: {
+                target: 'pino-pretty',
+            },
+        }),
+    );
+
+    app.use('*', (req, res, next) => {
+        res.status(404).json({
+            message: 'Not found',
+        })
+    })
+
+    const PORT = 3000;
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    })
 }
 
 
